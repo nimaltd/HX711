cfa07157fd3e1c950be3bce2fb0459710ee390a3
anon <anon@anon.anon>
2023-08-13 18:20:26 +0200

    Normalized formatting

--- hx711.h
+++ hx711.h
@@ -7,58 +7,59 @@
   WebSite:    http://www.github.com/NimaLTD
   Instagram:  http://instagram.com/github.NimaLTD
   Youtube:    https://www.youtube.com/channel/UCUhY7qY1klJm1d2kulr9ckw
-  
+
   Version:    1.1.1
-  
-  
+
+
   Reversion History:
-  
+
   (1.1.1):
-    Add power down/up.
+	Add power down/up.
   (1.1.0):
-    Add structure, Add calibration, Add weight, change names, ...
+	Add structure, Add calibration, Add weight, change names, ...
   (1.0.0):
-    First Release.
+	First Release.
 */
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-  
+
 #include "main.h"
 
-//####################################################################################################################
+	// ####################################################################################################################
 
-typedef struct
-{
-  GPIO_TypeDef  *clk_gpio;
-  GPIO_TypeDef  *dat_gpio;
-  uint16_t      clk_pin;
-  uint16_t      dat_pin;
-  int32_t       offset;
-  float         coef;
-  uint8_t       lock;    
-  
-}hx711_t;
+	typedef struct {
+			GPIO_TypeDef *clk_gpio;
+			GPIO_TypeDef *dat_gpio;
+			uint16_t	  clk_pin;
+			uint16_t	  dat_pin;
+			int32_t		  offset;
+			float		  coef;
+			uint8_t		  lock;
 
-//####################################################################################################################
+	} hx711_t;
 
-void        hx711_init(hx711_t *hx711, GPIO_TypeDef *clk_gpio, uint16_t clk_pin, GPIO_TypeDef *dat_gpio, uint16_t dat_pin);
-int32_t     hx711_value(hx711_t *hx711);
-int32_t     hx711_value_ave(hx711_t *hx711, uint16_t sample);
+	// ####################################################################################################################
 
-void        hx711_coef_set(hx711_t *hx711, float coef);
-float       hx711_coef_get(hx711_t *hx711);
-void        hx711_calibration(hx711_t *hx711, int32_t value_noload, int32_t value_load, float scale);
-void        hx711_tare(hx711_t *hx711, uint16_t sample);
-float       hx711_weight(hx711_t *hx711, uint16_t sample);
-void        hx711_power_down(hx711_t *hx711);
-void        hx711_power_up(hx711_t *hx711);
+	void	hx711_init(hx711_t *hx711, GPIO_TypeDef *clk_gpio, uint16_t clk_pin,
+		   GPIO_TypeDef *dat_gpio, uint16_t dat_pin);
+	int32_t hx711_value(hx711_t *hx711);
+	int32_t hx711_value_ave(hx711_t *hx711, uint16_t sample);
 
-//####################################################################################################################
+	void  hx711_coef_set(hx711_t *hx711, float coef);
+	float hx711_coef_get(hx711_t *hx711);
+	void  hx711_calibration(hx711_t *hx711, int32_t value_noload, int32_t value_load,
+		 float scale);
+	void  hx711_tare(hx711_t *hx711, uint16_t sample);
+	float hx711_weight(hx711_t *hx711, uint16_t sample);
+	void  hx711_power_down(hx711_t *hx711);
+	void  hx711_power_up(hx711_t *hx711);
+
+	// ####################################################################################################################
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif 
+#endif
--- hx711Config.h
+++ hx711Config.h
@@ -1,6 +1,6 @@
 #ifndef _HX711CONFIG_H_
 #define _HX711CONFIG_H_
 
-#define		_HX711_USE_FREERTOS		0
-#define   _HX711_DELAY_US_LOOP  4
-#endif 
+#define _HX711_USE_FREERTOS	 0
+#define _HX711_DELAY_US_LOOP 4
+#endif
